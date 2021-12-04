b1937f44385c20dd2b38b442c24743c8201edd97
FireStoneWoolfersWolf <82215907+FireStoneWoolfersWolf@users.noreply.github.com>
2021-12-04 11:08:48 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML = ""; 
+    recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+     console.log(event); 
+
+    var Content = event.results[0][0].transcript;
+
+    document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+    if(Content =="take my selfie"){
+        console.log("taking selfie");
+
+    speak();
+    }
+}
+
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    speak_data = "taking your selfie in 6 seconds";
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    Webcam.attach(camera);
+
+setTimeout(function(){
+    takeSnapshot();
+    save();
+
+}, 6000);
+}
+
+ 
+camera = document.getElementById("camera");
+Webcam.set({
+    width:360,
+    height:250,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
+
+function takeSnapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="SelfieOfYou" src="'+data_uri+'">';
+
+    });
+}
+function save(){
+    link = document.getElementById("link");
+    img = document.getElementById("SelfieOfYou").scroll;
+    link.href = image;
+    link.click()
+}
